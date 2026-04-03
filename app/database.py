a08d3789ffa978b8133eb29f71804cf70aff5aa2
yanfan <ewan4434@gmail.com>
2026-04-03 16:05:26 -0700

    PostgreSQL connection set up

--- app/database.py
+++ app/database.py
@@ -8,3 +8,22 @@ from app.config import settings
 def get_db_connection():
     # Return a new PostgreSQL connection using DATABASE_URL
     return psycopg.connect(settings.DATABASE_URL)
+
+
+def test_db_connection():
+    # open a database connection
+    conn = get_db_connection()
+
+    # create a cursor to test sql
+    cur = conn.cursor()
+
+    # Run a simple test query
+    cur.execute("SELECT 123;")
+
+    # Get result
+    result = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return result
